b2a16dbe86bfb6d0ce50e577a0d859fa280d70f4
Marcin Piczak <marcin.piczak@gmail.com>
2018-03-17 12:49:00 +0100

    szkielet - poprawki

--- Warsztaty/Tasks/ConsoleEx.cs
+++ Warsztaty/Tasks/ConsoleEx.cs
@@ -6,19 +6,19 @@ namespace Tasks
 {
     public static class ConsoleEx
     {
-        public static void WriteLine(string message, ConsoleColor color)
+        public static void WriteLine(string message, ConsoleColor color, string[] args = null)
         {
             var colorOld = Console.ForegroundColor;
             Console.ForegroundColor = color;
-            Console.WriteLine(message);
+            if (args != null) Console.WriteLine(message, args);
             Console.ForegroundColor = colorOld;
         }
 
-        public static void Write(string message, ConsoleColor color)
+        public static void Write(string message, ConsoleColor color, string[] args = null)
         {
             var colorOld = Console.ForegroundColor;
             Console.ForegroundColor = color;
-            Console.Write(message);
+            if (args != null) Console.Write(message, args);
             Console.ForegroundColor = colorOld;
         }
     }
