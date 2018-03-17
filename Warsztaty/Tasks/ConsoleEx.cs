3de4352664007a60aa789ddb2b1f4a269df70782
Marcin Piczak <marcin.piczak@gmail.com>
2018-03-17 13:04:41 +0100

    ConsoleEx - poprawki

--- Warsztaty/Tasks/ConsoleEx.cs
+++ Warsztaty/Tasks/ConsoleEx.cs
@@ -6,21 +6,19 @@ namespace Tasks
 {
     public static class ConsoleEx
     {
-        public static void WriteLine(string message, ConsoleColor color, string[] args = null)
+        public static void WriteLine(string message, ConsoleColor color, params object[] args)
         {
             var colorOld = Console.ForegroundColor;
             Console.ForegroundColor = color;
-            if (args != null) Console.WriteLine(message, args);
-            else Console.WriteLine(message);
+            Console.WriteLine(message, args);
             Console.ForegroundColor = colorOld;
         }
 
-        public static void Write(string message, ConsoleColor color, string[] args = null)
+        public static void Write(string message, ConsoleColor color, params object[] args)
         {
             var colorOld = Console.ForegroundColor;
             Console.ForegroundColor = color;
-            if (args != null) Console.Write(message, args);
-            else Console.Write(message);
+            Console.Write(message, args);
             Console.ForegroundColor = colorOld;
         }
     }
